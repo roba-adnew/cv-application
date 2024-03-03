9a70ecd5d87fa30ed330ba58ede2da24d8e2a0c7
roba <10408936+roba-adnew@users.noreply.github.com>
2024-03-03 09:56:25 -0500

    Decoupled data and corresponding html tags and attributes

--- cv-application/src/cvBuilder.jsx
+++ cv-application/src/cvBuilder.jsx
@@ -4,55 +4,71 @@ import './cvBuilder.css';
 export default function HeaderForm() {
     const [shouldRenderInput, setShouldRenderInput] = useState(true);
     const [header, setHeader] = useState(() => ({
-        name: {
-            placeholder: 'Full Name',
-            required: true,
-            value: ''
-        },
-        phone: {
-            placeholder: 'Phone Number',
-            required: true,
-            value: ''
-        },
-        email: {
-            placeholder: 'Email',
-            required: true,
-            value: ''
-        },
-        address: {
-            placeholder: 'Address',
-            required: true,
-            value: ''
-        },
-        linkedin: {
-            placeholder: 'LinkedIn',
-            required: true,
-            value: ''
-        },
-        github: {
-            placeholder: 'Github',
-            required: false,
-            value: ''
-        }
+        name: '',
+        phone: '',
+        email: '',
+        address: '',
+        linkedin: '',
+        github: ''
     }))
+
+    const tagAttributes = 
+        {
+            name: {
+                placeholder: 'Full Name',
+                required: true,
+                type: 'text'
+            },
+            phone: {
+                placeholder: 'Phone Number',
+                required: true,
+                type: 'text'
+            },
+            email: {
+                placeholder: 'Email',
+                required: true,
+                type: 'email'
+            },
+            address: {
+                placeholder: 'Address',
+                required: true,
+                type: 'text'
+            },
+            linkedin: {
+                placeholder: 'LinkedIn',
+                required: true,
+                type: 'url'
+            },
+            github: {
+                placeholder: 'Github',
+                required: false,
+                type: 'url'
+            }
+        };
      
 
     function updateHeader(e) {
-        e.preventDefault();``
+        e.preventDefault();
         const value = e.target.value;
-        const field = e.target.id;
-        setHeader({...header, [field]: {
-            ...header[field], value: value
-        }});
+        const field = e.target.key;
+        setHeader({...header, [field]: value});
     }
 
     function handleEdit(e) {
         e.preventDefault();
         setShouldRenderInput(!shouldRenderInput);
     }
-
     const shouldShowSubmission = !shouldRenderInput;
+
+    const dataAndTagStructureMatch = Object.keys(header)
+        .every(field => {
+            return Object.keys(tagAttributes).includes(field) 
+        })
     
+    if (!dataAndTagStructureMatch) {
+        throw new Error('The DOM may not match the structure of the header data')
+    }
+
     return (
         <div>
             <p>Resume Header</p>
@@ -60,10 +76,10 @@ export default function HeaderForm() {
                     <form>
                         {Object.keys(header).map(field => 
                             <input 
-                                placeholder={header[field]['placeholder']}
+                                placeholder={tagAttributes[field]['placeholder']}
                                 key={field}
-                                id={field}
-                                {...header[field]['required'] 
+                                type={tagAttributes[field]['placeholder']}
+                                {...tagAttributes[field]['required'] 
                                     && {required: true}}
                                 onChange={updateHeader}
                                 >
@@ -77,7 +93,7 @@ export default function HeaderForm() {
                 {shouldShowSubmission && 
                     <div>
                         {Object.keys(header).map(field => 
-                            <p key={field}>{header[field]['value']}</p>
+                            <p key={field}>{header[field]}</p>
                         )}
                         <button onClick={handleEdit}>Edit</button>
                     </div>
